078305a9a11f81a959b8a7aa917c779f7a4b593b
DuongMinhh <67991904+DuongMinhh@users.noreply.github.com>
2021-08-22 20:04:08 +0700

    Test merge

--- MyApplication-WebService/src/main/java/com/me/webservice/MyApplication.java
+++ MyApplication-WebService/src/main/java/com/me/webservice/MyApplication.java
@@ -25,6 +25,7 @@ public class MyApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MyApplication.class, args);
+		System.out.println("Test - merge: Main");
 	}
 
 	@Autowired
